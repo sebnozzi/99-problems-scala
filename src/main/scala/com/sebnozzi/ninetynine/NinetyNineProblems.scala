950e364cbb79a78f38a88b2e9dd9d12d49e66b72
Sebastian Nozzi <sebnozzi@gmail.com>
2014-01-17 23:09:46 +0100

    P05 / P06 - Moved functions to main object

--- src/main/scala/com/sebnozzi/ninetynine/NinetyNineProblems.scala
+++ src/main/scala/com/sebnozzi/ninetynine/NinetyNineProblems.scala
@@ -1,5 +1,56 @@
 package com.sebnozzi.ninetynine
 
-object NinetyNineProblems extends App {
-  println("Hello, 99 Problems")
+object NinetyNineProblems {
+
+  // P01 - should find the last element of a list
+  def last[T](list: List[T]): T =
+    list match {
+      case List(only) => only
+      case first :: rest => last(rest)
+      case Nil => error("An empty list has no last element")
+    }
+
+  // P02 - find the penultimate element of a list
+  def penultimate[T](list: List[T]): T =
+    list match {
+      case List(first, last) => first
+      case first :: second :: rest => penultimate(second :: rest)
+      case _ => error("Can not find penultimate of this list: " + list)
+    }
+
+  // P03 - find the Nth element of a list
+  def nth[T](n: Int, list: List[T]): T =
+    if (n == 0)
+      list.head
+    else
+      nth(n - 1, list.tail)
+
+  // P04 - find the number of elements of a list (recursive)
+  def length[T](list: List[T]): Int =
+    list match {
+      case Nil => 0
+      case x :: rest => 1 + length(rest)
+    }
+
+  // P04 - find the number of elements of a list (folding)
+  def lengthFolding[T](list: List[T]): Int =
+    list.foldLeft(0)((count, _) => 1 + count)
+
+  // P05 - reverse a list (recursive)
+  def reverse[T](list: List[T]): List[T] = {
+    def helper(remaining: List[T], inverted: List[T]): List[T] =
+      remaining match {
+        case Nil => inverted
+        case first :: rest => helper(rest, first :: inverted)
+      }
+    helper(list, Nil)
+  }
+
+  // P05 - reverse a list (folding)
+  def reverseFolding[T](list: List[T]): List[T] =
+    list.foldLeft(List.empty[T])((result, element) => element :: result)
+
+  // P06 - find out whether a list is palyndrome
+  def isPalyndrome[T](list: List[T]) = reverse(list) == list
+
 }
--- src/test/scala/com/sebnozzi/ninetynine/NinetyNineProblemsSuite.scala
+++ src/test/scala/com/sebnozzi/ninetynine/NinetyNineProblemsSuite.scala
@@ -1,57 +1,55 @@
 package com.sebnozzi.ninetynine
 
-import org.scalatest.FunSuite
 import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
 import org.scalatest.Matchers
 
-@RunWith(classOf[JUnitRunner])
-// }}
 class NinetyNineProblemsSuite extends FunSuite with Matchers {
-  
+
+  import NinetyNineProblems._
+
   test("P01 - should find the last element of a list") {
-    def last[T](list: List[T]): T =
-      list match {
-        case x :: Nil => x
-        case x :: rest => last(rest)
-        case Nil => error("An empty list has no last element")
-      }
-    last(List(1, 2, 3, 5, 8)) should be(8)
+    last(List(1, 2, true, "five", 8)) should be(8)
     last(List(7)) should be(7)
   }
 
   test("P02 - find the penultimate element of a list") {
-    def penultimate[T](list: List[T]): T =
-      list match {
-        case first :: last :: Nil => first
-        case first :: second :: rest => penultimate(second :: rest)
-        case _ => error("Can not find penultimate of this list: " + list)
-      }
-
     penultimate(List(1, 2, 3, 5, 8)) should be(5)
     penultimate(List(1, 3)) should be(1)
   }
 
   test("P03 - find the Nth element of a list") {
-    def nth[T](n: Int, list: List[T]): T =
-      if (n == 0)
-        list.head
-      else
-        nth(n - 1, list.tail)
-
     nth(3, List(1, 1, 3, 5, 8)) should be(5)
     nth(0, List(1, 1, 3, 5, 8)) should be(1)
   }
 
   test("P04 - find the number of elements of a list") {
-    def length[T](list: List[T]): Int =
-      list match {
-        case Nil => 0
-        case x :: rest => 1 + length(rest)
-      }
-
-    length(List(1, 1, 2, 3, 5, 7)) should be(6)
+    import NinetyNineProblems.{ length => myLength }  // conflicts with Matchers :-(
+    myLength(List(1, 1, 2, 3, 5, 7)) should be(6)
+    lengthFolding(List(1, 1, 2, 3, 5, 7)) should be(6)
   }
 
+  test("P05 - reverse a list") {
+    def reverse[T](list: List[T]): List[T] = {
+      def helper(remaining: List[T], inverted: List[T]): List[T] =
+        remaining match {
+          case Nil => inverted
+          case first :: rest => helper(rest, first :: inverted)
+        }
+      helper(list, Nil)
+    }
+
+    def reverseFolding[T](list: List[T]): List[T] =
+      list.foldLeft(List.empty[T])((result, element) => element :: result)
+
+    reverse(List(1, 2, 3, 4)) should be(List(4, 3, 2, 1))
+    reverseFolding(List(1, 2, 3, 4)) should be(List(4, 3, 2, 1))
+  }
   
+  test("P06 - find out whether a list is palyndrome") {
+    isPalyndrome(List(1,2,3)) should be (false)
+    isPalyndrome(List(1,2,2,1)) should be (true)
+    isPalyndrome(List(5)) should be (true)
+  }
+
 }
